39976a6aefa7b301559bded5782ba976e02a20b6
서가희 <kahee430@gmail.com>
2024-03-19 15:15:49 +0900

    [style #43] - 메인페이지 UI 수정

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -12,13 +12,13 @@ export default function HomePage() {
           <img src="assets/logo.png" alt="logo" className="w-3/6 mt-2" />
         </div>
         <div className="w-72 flex justify-evenly items-center text-l">
-          <p className="cursor-pointer hover:text-slate-600 font-bold">회원가입</p>
-          <p className="cursor-pointer hover:text-slate-600 font-bold">로그인</p>
+          <p className="cursor-pointer hover:text-zinc-500 font-bold">회원가입</p>
+          <p className="cursor-pointer hover:text-zinc-500 font-bold">로그인</p>
         </div>
       </div>
       <div className="w-full flex justify-center gap-5 mb-5">
         <input type="text" placeholder="검색어를 입력하세요" className="flex w-3/12 p-1 w-3/10 border rounded-md" />
-        <button className="hover:text-slate-600 font-bold">검색</button>
+        <button className="hover:text-zinc-500 font-bold">검색</button>
       </div>
       <div className="carousel w-full h-80">
         <div id="slide1" className="carousel-item relative w-full">
@@ -68,21 +68,21 @@ export default function HomePage() {
       </div>
 
       <div className="flex justify-evenly w-4/5 p-10 m-auto text-xl font-bold">
-        <p className="flex  gap-3 items-center cursor-pointer hover:text-slate-600">
+        <p className="flex  gap-3 items-center cursor-pointer hover:text-zinc-500">
           <GiFruitBowl /> 과일/채소
         </p>
-        <p className="flex items-center gap-3 cursor-pointer hover:text-slate-600">
+        <p className="flex items-center gap-3 cursor-pointer hover:text-zinc-500">
           <TbMeat /> 고기
         </p>
-        <p className="flex gap-3 items-center cursor-pointer hover:text-slate-600">
+        <p className="flex gap-3 items-center cursor-pointer hover:text-zinc-500">
           <GiHamburger /> 가공식품
         </p>
-        <p className="flex gap-3 items-center cursor-pointer hover:text-slate-600">
+        <p className="flex gap-3 items-center cursor-pointer hover:text-zinc-500">
           <IoFishOutline /> 해산물
         </p>
       </div>
       <div className="w-4/5 flex justify-end font-bold mb-5">
-        <span className="hover:text-cyan-400 mr-2 cursor-pointer">신상품</span> ||{' '}
+        <span className="hover:text-cyan-400 mr-2 cursor-pointer">신상품</span> ||
         <span className="hover:text-pink-300 mx-2 cursor-pointer">베스트</span>
       </div>
       <div className="flex justify-center w-4/5 m-auto gap-20">
