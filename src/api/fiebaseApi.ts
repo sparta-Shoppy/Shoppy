54e0498a823297e2e25a9b48a59ce38de4b2caa1
porosadporosad <tmxk1594@naver.com>
2024-03-19 21:46:30 +0900

    [Fix #16] ApiKey 수정

--- src/api/fiebaseApi.ts
+++ src/api/fiebaseApi.ts
@@ -10,11 +10,11 @@ export let app: FirebaseApp;
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_APP_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_APP_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_APP_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_APP_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_APP_APP_ID,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
   databaseURL: process.env.NEXT_PUBLIC_DATABASE_URL
 };
 
