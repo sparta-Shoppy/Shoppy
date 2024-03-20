2715853e2280396846f859a413c6a9fd88a783be
서가희 <kahee430@gmail.com>
2024-03-20 23:04:53 +0900

    [feat] - 상품 리스트 금액별 정렬 구현

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -4,7 +4,7 @@ import Header from '@/components/common/Header';
 import SearchProduct from '@/components/common/SearchProduct';
 import { stringTransform } from '@/hooks/transform';
 import { ProductType } from '@/types/product-type';
-import { collection, getDocs, query } from 'firebase/firestore';
+import { collection, getDocs } from 'firebase/firestore';
 import Link from 'next/link';
 import { useEffect, useState } from 'react';
 import { GiFruitBowl, GiHamburger } from 'react-icons/gi';
@@ -24,19 +24,27 @@ export default function HomePage() {
         const fetchedProducts: any = [];
 
         querySnapshot.forEach((doc) => {
-          const products = doc.data().newProduct;
+          const products = doc.data();
           fetchedProducts.push({ ...products, id: doc.id, products });
         });
         if (fetchedProducts.length != null) {
         }
-        setProducts(fetchedProducts);
+
+        const sortedProducts =
+          selectedTab === '신상품'
+            ? fetchedProducts.sort(
+                (a: any, b: any) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
+              )
+            : fetchedProducts.sort((a: any, b: any) => b.price - a.price);
+
+        setProducts(sortedProducts);
       } catch (error) {
         console.log('상품 데이터 가져오기 실패', error);
       }
     };
 
     fetchProductsData();
-  }, []);
+  }, [selectedTab]);
 
   return (
     <div className="h-screen">
@@ -146,24 +154,26 @@ export default function HomePage() {
         </span>
       </div>
       <div className="flex justify-center w-4/5 m-auto gap-20 flex-wrap">
-        {products.map((item) => (
-          <div
-            key={item.productId}
-            className="w-1/4 h-96 flex flex-wrap justify-center cursor-pointer rounded-md hover:shadow-lg hover:shadow hover:scale-110 transition-all duration-300 pb-5"
-          >
-            <img src={item.image} alt="상품" className="w-full h-4/5 object-cover rounded-tl-md rounded-tr-md" />
-            <div className="flex justify-between p-3">
-              <div>
-                <p>{item.title}</p>
-                <p>{stringTransform(item.price)}</p>
-              </div>
-              <div className="flex justify-end gap-2 items-center pl-5">
-                <SlHeart className="text-2xl hover:text-rose-500 cursor-pointer" />
-                <LiaCartArrowDownSolid className="text-4xl hover:text-stone-300 cursor-pointer" />
+        {products?.map((item) => {
+          return (
+            <div
+              key={item.productId}
+              className="w-1/4 h-96 flex flex-wrap justify-center cursor-pointer rounded-md hover:shadow-lg hover:shadow hover:scale-110 transition-all duration-300 pb-5"
+            >
+              <img src={item.image} alt="상품" className="w-full h-4/5 object-cover rounded-tl-md rounded-tr-md" />
+              <div className="flex justify-between p-3">
+                <div>
+                  <p>{item.title}</p>
+                  <p>{stringTransform(item.price)}</p>
+                </div>
+                <div className="flex justify-end gap-2 items-center pl-5">
+                  <SlHeart className="text-2xl hover:text-rose-500 cursor-pointer" />
+                  <LiaCartArrowDownSolid className="text-4xl hover:text-stone-300 cursor-pointer" />
+                </div>
               </div>
             </div>
-          </div>
-        ))}
+          );
+        })}
       </div>
 
       <div className="flex justify-center">
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -23,43 +23,35 @@ export default function ProductPage() {
     const fetchProductsData = async () => {
       try {
         const querySnapshot = await getDocs(
-          query(
-            collection(db, 'product'),
-            where('newProduct.category', '==', `${params}`),
-            orderBy('newProduct.price', 'desc')
-          )
+          query(collection(db, 'product'))
+          // query(collection(db, 'product'), where('newProduct', '==', `${params}`))
+          // query(collection(db, 'product'), where('newProduct', '==', `${params}`), orderBy('newProduct', 'desc'))
         );
         console.log('querySnapshot', querySnapshot);
-        const fetchedProducts: any = [];
+        const fetchedProducts: any[] = [];
 
         querySnapshot.forEach((doc) => {
-          console.log('doc', doc);
-          const products = doc.data().newProduct;
+          const products = doc.data();
           fetchedProducts.push({ ...products, id: doc.id, products });
         });
         if (fetchedProducts.length != null) {
         }
 
         console.log('fetchedProducts', fetchedProducts);
-        setProducts(fetchedProducts);
+
+        const sortedProducts =
+          selectedTab === '높은 가격순'
+            ? fetchedProducts.sort((a: any, b: any) => b.price - a.price)
+            : fetchedProducts.sort((a: any, b: any) => a.price - b.price);
+
+        setProducts(sortedProducts);
       } catch (error) {
         console.log('상품 데이터 가져오기 실패', error);
       }
     };
 
     fetchProductsData();
-  }, []);
-
-  const productsSort = () => {
-    if (selectedTab === '높은 가격순') {
-      setSelectedTab('높은 가격순');
-      // const productsAsc = query(citiesRef, orderBy('name', 'desc'));
-    }
-
-    if (selectedTab === '낮은 가격순') {
-      setSelectedTab('낮은 가격순');
-    }
-  };
+  }, [selectedTab]);
 
   return (
     <div>
@@ -70,7 +62,7 @@ export default function ProductPage() {
           className={`cursor-pointer mr-2 ${
             selectedTab === '높은 가격순' ? 'text-zinc-400' : 'text-black hover:text-zinc-400'
           }`}
-          onClick={productsSort}
+          onClick={() => setSelectedTab('높은 가격순')}
         >
           높은 가격순
         </span>
@@ -79,38 +71,45 @@ export default function ProductPage() {
           className={`cursor-pointer mx-2 ${
             selectedTab === '낮은 가격순' ? 'text-zinc-400' : 'text-black hover:text-zinc-400'
           }`}
-          onClick={productsSort}
+          onClick={() => setSelectedTab('낮은 가격순')}
         >
           낮은 가격순
         </span>
       </div>
       <div className="m-auto w-full flex flex-wrap justify-center">
-        {products.map((item) => (
-          <div key={item.productId} className="w-1/4 p-5">
-            <div className="h-full flex flex-col justify-between items-center ">
-              <img
-                src={item.image}
-                alt="상품이미지"
-                className="w-4/5 h-80 object-cover rounded-md hover:scale-95 transition-all duration-300 cursor-pointer"
-              />
-              <button className="w-4/5 mt-3 bg-slate-100 flex justify-center items-center text-black py-2 px-4 hover:bg-white rounded-md">
-                <span className="text-xl">담기</span> <LiaCartArrowDownSolid className="ml-2 text-3xl" />
-              </button>
-              <div className="pt-2 w-4/5">
-                <p className="text-lg font-semibold">{item.title}</p>
-                <div className="flex justify-between items-center mt-2">
-                  <div>
-                    <p className="text-lg font-semibold">{stringTransform(item.price)} 원</p>
-                  </div>
-                  <div className="flex gap-2 items-center">
-                    <SlHeart className="text-3xl hover:text-rose-500 cursor-pointer" />
+        {products?.map((item) => {
+          return (
+            <div key={item.productId} className="w-1/4 p-5">
+              <div className="h-full flex flex-col justify-between items-center ">
+                <img
+                  src={item.image}
+                  alt="상품이미지"
+                  className="w-4/5 h-80 object-cover rounded-md hover:scale-95 transition-all duration-300 cursor-pointer"
+                />
+                <button className="w-4/5 mt-3 bg-slate-100 flex justify-center items-center text-black py-2 px-4 hover:bg-white rounded-md">
+                  <span className="text-xl">담기</span> <LiaCartArrowDownSolid className="ml-2 text-3xl" />
+                </button>
+                <div className="pt-2 w-4/5">
+                  <p className="text-lg font-semibold">{item.title}</p>
+                  <div className="flex justify-between items-center mt-2">
+                    <div>
+                      <p className="text-lg font-semibold">{item.price ? stringTransform(item.price) : null} 원</p>
+                    </div>
+                    <div className="flex gap-2 items-center">
+                      <SlHeart className="text-3xl hover:text-rose-500 cursor-pointer" />
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
-        ))}
+          );
+        })}
       </div>
     </div>
   );
 }
+
+// 파이어베이스에서 orderby로 내림차순 정렬 시 금액이 문자열이기때문에 정렬이 잘 안됨 그래서 받아온 데이터를 사용하는 곳에서 sort로 메서드 체이닝을함
+// 파이어베이스에서 orderby로 내림차순 정렬하는 방법이 궁금함
+
+// 홈 화면 탭 클릭하면 params을 리스트페이지에 잘 넘겨주지만 파이어베이스에서 조회를 못함
