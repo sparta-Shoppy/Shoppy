8649aa80de47cd95f021f74cd4def4def47c4fec
이준구 <leejungoo1396@gmail.com>
2024-03-23 03:47:51 +0900

    [Style #32] 마이페이지 Ui 구성

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,3 +1,64 @@
+import Header from '@/components/common/Header';
+import { IoHeartSharp } from 'react-icons/io5';
+
 export default function ProfilePage() {
-  return <div>ProfilePage</div>;
+  return (
+    <>
+      <Header />
+      <div className="w-full h-full p-20 bg-slate-600">
+        {/* user 정보 */}
+        <h1 className="pt-24 text-center tex text-5xl">회원정보</h1>
+        <div className="p-24">
+          <div className="flex flex-col gap-1 mb-20">
+            <p className="text-2xl">이메일 입니다.</p>
+            <p className="text-2xl">닉네임 입니다.</p>
+          </div>
+          {/* user가 찜한 데이터 */}
+          <div className="flex items-center gap-5 mb-10">
+            {/* {heart ? ' text-rose-500' : ' hover:text-rose-500'} */}
+            <IoHeartSharp className={`text-6xl  text-rose-500`} />
+            <p className="text-3xl">찜 목록 개수</p>
+          </div>
+          <div className="flex bg-white p-4 mb-32">
+            {/* 상품 이미지 */}
+            <IoHeartSharp className="w-80 h-40"></IoHeartSharp>
+            {/* <img src={img} alt={title} className="w-60 h-60" /> */}
+
+            <p className="flex-col justify-items-center ">
+              <span className="block">제목</span>
+              <span className="block">가격</span>
+            </p>
+          </div>
+          {/* user가 작성한 후기 내용 */}
+          <div className="flex items-center gap-5 mb-10">
+            {/* {heart ? ' text-rose-500' : ' hover:text-rose-500'} */}
+            <IoHeartSharp className={`text-6xl  text-rose-500`} />
+            <p className="text-3xl">작성한 후기 개수</p>
+          </div>
+          <div className="flex-col bg-white p-6 mb-10">
+            <p className="flex gap-40">
+              <span>제목입니다.</span>
+              <span>시간입니다.</span>
+            </p>
+
+            <p>내용</p>
+          </div>
+          {/* user가 작성한 문의 내용 */}
+          <div className="flex items-center gap-5 mb-10">
+            {/* {heart ? ' text-rose-500' : ' hover:text-rose-500'} */}
+            <IoHeartSharp className={`text-6xl  text-rose-500`} />
+            <p className="text-3xl">작성한 리뷰 개수</p>
+          </div>
+          <div className="flex-col bg-white p-6 mb-10">
+            <p className="flex gap-40">
+              <span>제목입니다.</span>
+              <span>시간입니다.</span>
+            </p>
+
+            <p>내용</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
